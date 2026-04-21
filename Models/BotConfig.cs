470598621b7f97fa33e251e07d853beaa7fd4ccd
Demian <konoplev.dm.06@gmail.com>
2026-04-21 15:07:02 +0300

    [feat] Added Bot models

---
+++ Models/BotConfig.cs
@@ -0,0 +1,55 @@
+namespace BybitBot.Models;
+
+/// <summary>
+/// Текущее состояние бота
+/// </summary>
+public class BotState
+{
+    public decimal? ReferencePrice { get; set; }     
+    public bool HasOpenPosition { get; set; }         
+    public decimal? AverageEntryPrice { get; set; }        
+    public PositionType? CurrentPositionType { get; set; } // Long или Short
+    
+    // Статистика
+    public int TotalTrades { get; set; }
+    public decimal TotalProfit { get; set; }
+    
+    // Для отладки
+    public DateTime LastUpdateTime { get; set; }
+    public decimal LastPrice { get; set; }
+    
+    public void Reset()
+    {
+        ReferencePrice = null;
+        HasOpenPosition = false;
+        AverageEntryPrice = null;
+        CurrentPositionType = null;
+    }
+    
+    public void RecordTrade(PositionType type, decimal price, decimal profit = 0)
+    {
+        HasOpenPosition = true;
+        CurrentPositionType = type;
+        AverageEntryPrice = price;
+        ReferencePrice = price;
+        TotalTrades++;
+        TotalProfit += profit;
+    }
+    
+    public void ClosePosition(decimal exitPrice, decimal profit)
+    {
+        HasOpenPosition = false;
+        CurrentPositionType = null;
+        ReferencePrice = exitPrice;
+        TotalProfit += profit;
+    }
+}
+
+/// <summary>
+/// Тип позиции
+/// </summary>
+public enum PositionType
+{
+    Long,   // Покупка (ожидаем рост)
+    Short   // Продажа (ожидаем падение)
+}
